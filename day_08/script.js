771d58633bd4a36f9245f2df4a5bd7c55dffe7e1
Mohamed Younis <younissm602@gmail.com>
2025-08-08 17:07:17 +0300

    Update script for transition delay for Day 08

--- day_08/script.js
+++ day_08/script.js
@@ -5,7 +5,7 @@ labels.forEach((label) => {
     .split("")
     .map(
       (letter, idx) =>
-        `<span style="transition-delay:${idx * 5000}ms">${letter}</span>`
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
     )
     .join("");
 });
